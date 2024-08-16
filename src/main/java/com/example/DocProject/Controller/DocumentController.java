54036740aae5cdb7e74e18492fcd22b349f79c88
Kurtburakk <b.kurt2021@gtu.edu.tr>
2024-08-16 16:30:40 +0300

    add pdf to word conversion and minor change document service

--- src/main/java/com/example/DocProject/Controller/DocumentController.java
+++ src/main/java/com/example/DocProject/Controller/DocumentController.java
@@ -1,32 +1,31 @@
 package com.example.DocProject.Controller;
 
 import com.example.DocProject.Service.DocumentService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.File;
 import java.io.IOException;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/documents")
 public class DocumentController {
 
-    @Autowired
     private DocumentService documentService;
+
 
     @PostMapping("/generatePdf")
     public String generatePdf(
             @RequestParam("templateName") String templateName,
-            @RequestParam("outputName") String outputName,
             @RequestBody JsonNode jsonData) throws IOException {
 
         File templateFile = new File("uploads/" + templateName +".docx"); // it holds to word file path
 
-        File outputFile = new File("uploads/" + outputName + ".pdf");   // it holds to new pdf name and path
+        documentService.processWordTemplate(templateFile, jsonData);
 
-        documentService.processWordTemplate(templateFile, jsonData, outputFile);
-
-        return "PDF oluşturuldu: " + outputFile.getAbsolutePath();
+        return "Word filled with name : " + templateName + "_filled.docx";
 
     }
 }
